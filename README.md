3ed73069d03c3d16576bf70a4627a1e1c7946e45
Olha-Suchok <suchok_olya@ukr.net>
2023-02-13 20:18:52 +0200

    Completed tasks

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # CLI
+
+node index.js --action="list" - https://monosnap.com/file/6mJ6TfweFAxjyiRSvkGBt8OveVqY5P
+node index.js --action="get" --id=5 - https://monosnap.com/file/dcSVUmCSBvLjG2AXjnNY4AxTtjRR20
+node index.js --action="add" --name="Mango" --email="mango@gmail.com" --phone="322-22-22" - https://monosnap.com/file/YT21xk0nLZpuXdyMDdRo6E6PZ72laU
+node index.js --action="remove" --id=3 - https://monosnap.com/file/L1AXGZGCvowW3QqU1OcBMmQnokSVYK
--- contacts.js
+++ contacts.js
@@ -1,32 +1,76 @@
+const crypro = require("crypto");
 const fs = require("fs").promises;
 const path = require("path");
-// console.log(fs);
-// console.log(path);
+require("colors");
 
-const contactsPath = path.join("db/contacts.json");
-console.log(contactsPath);
+const contactsPath = path.join(__dirname, "./db/contacts.json");
 
 async function listContacts() {
   try {
-    console.log("Отримали список контактів");
     const response = await fs.readFile(contactsPath);
-    const data = JSON.parse(response);
-    console.table(data);
+    const contacts = JSON.parse(response);
+    console.log("Отримали список контактів:".green);
+    console.table(contacts);
   } catch (error) {
-    console.log(error);
+    console.error(err.message);
   }
 }
 
-function getContactById(contactId) {
-  console.log("Отримали контакт по його id");
+async function getContactById(contactId) {
+  try {
+    const response = await fs.readFile(contactsPath);
+    const contacts = JSON.parse(response);
+    const findContact = contacts.find((item) => item.id === contactId);
+
+    if (!findContact) {
+      return null;
+    }
+    console.log("Отримали контакт по його id:".green);
+    console.table(findContact);
+  } catch (error) {
+    console.error(err.message);
+  }
 }
 
-function removeContact(contactId) {
-  console.log("Видалили контакт по його id");
+async function removeContact(contactId) {
+  try {
+    const response = await fs.readFile(contactsPath);
+    const contacts = JSON.parse(response);
+    const newContactList = contacts.filter((item) => item.id !== contactId);
+    console.log("Видалений контакт по його id:".green);
+    console.table(newContactList);
+  } catch (error) {
+    console.error(err.message);
+  }
 }
 
-function addContact(name, email, phone) {
-  console.log("Додали контакт по його id");
+async function addContact(name, email, phone) {
+  try {
+    const newContact = { id: crypro.randomUUID(), name, email, phone };
+    const response = await fs.readFile(contactsPath);
+    const contacts = JSON.parse(response);
+
+    const isContact = contacts.find(
+      (contact) => contact.name === newContact.name
+    );
+
+    if (isContact) {
+      console.log("Контакт з таким імя'м вже зареєстровнний.".red);
+      return;
+    } else {
+      const newContactList = JSON.stringify(
+        [...contacts, newContact],
+        null,
+        "\t"
+      );
+      fs.writeFile(contactsPath, newContactList);
+      const newList = JSON.parse(newContactList);
+      console.log("Додали контакт по його id:".green);
+      console.table(newList);
+    }
+  } catch (error) {
+    console.error(err.message);
+  }
 }
 
 module.exports = {
